8d99ecece9b1fd1851cc90f65416d9a1fde078ce
Yusuke Oshiro <yusuke@metroly.io>
2020-10-27 23:26:15 +0900

    implemented matchTargetKeys

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,41 @@
 import _ from 'lodash'
 
+export function matchTargetKeys (target: any, input: any, nest: string[] = []): any {
+  const nestClone = nest.slice() // clone nest for each element
+
+  if (_.isArray(target) && _.isArray(input)) {
+    const newTarget = target[0] // use the first element as the target
+    try {
+      const output = input.map(element => {
+        return matchTargetKeys(newTarget, element, nestClone)
+      })
+      return output
+    } catch (error) {
+      return undefined
+    }
+  } else if (_.isObject(target) && _.isObject(input)) {
+    try {
+      const keys = Object.keys(target)
+      const picked = _.pick(input, keys) as any
+      const matchedInput =  _.mapValues(picked, (newInput, key) => {
+        const newTarget: any = (target as any)[key]
+        return matchTargetKeys(newTarget, newInput, [...nestClone, key])
+      })
+      if (Object.keys(matchedInput).length === 0) throw new Error('no key!')
+
+      return JSON.parse(JSON.stringify(matchedInput)) // eliminate keys with no value (undefined)
+    } catch (error) {
+      return undefined
+    }
+  } else {
+    const isRoot = (nestClone.length === 1)
+    if (typeof target !== typeof input &&  isRoot) return undefined
+    if (typeof target !== typeof input && !isRoot) throw new Error('different type')
+
+    return input
+  }
+}
+
 
 /**
  * @param object object to convert
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,9 +1,69 @@
 import { describe } from 'mocha'
 import * as chai from 'chai'
-import {snakeCase, camelCase} from '../src/index'
+import {snakeCase, camelCase, matchTargetKeys} from '../src/index'
 
 const assert = chai.assert
 const expect = chai.expect
+
+describe('object-manipulator', () => {
+  it('should reduce the keys to match the taret', () => {
+    const targetObject = {
+      a: 1,
+      c: {
+        c1: 1,
+        c3: {
+          c32: 1
+        },
+        c4: [1],
+        c5: ['a'],
+        c6: [
+          {
+            c61: 1,
+          }
+        ]
+      },
+      d: 'a'
+    }
+
+    const inputObject = {
+      a: 1,
+      b: 1, // remove
+      c: {
+        c1: 1,
+        c2: 2, // remove
+        c3: { // remove because childless
+          c31: 1 // remove
+        },
+        c4: [ 1,2,3],
+        c5: [ 1,2,3], // remove because different type
+        c6: [
+          {
+            c61: 1,
+            c62: 2, // remove
+          },
+          {
+            c61: 1,
+            c62: 2, // remove
+          }
+        ]
+      },
+      d: 1 // remove because different type
+    }
+
+    const matchedObject: any = matchTargetKeys(targetObject, inputObject)
+    expect(matchedObject['a']).to.equal(1)
+    expect(matchedObject['b']).to.equal(undefined)
+    expect(matchedObject['c']['c1']).to.equal(1)
+    expect(matchedObject['c']['c2']).to.equal(undefined)
+    expect(matchedObject['c']['c4']).to.be.instanceOf(Array)
+    expect(matchedObject['c']['c5']).to.equal(undefined)
+    expect(matchedObject['c']['c6'][0]['c61']).to.equal(1)
+    expect(matchedObject['c']['c6'][0]['c62']).to.equal(undefined)
+    expect(matchedObject['c']).to.not.have.property('c5')
+    expect(matchedObject['c']).to.not.have.property('c3')
+    expect(matchedObject).to.not.have.property('d')
+  })
+})
 
 describe('case-converter', () => {
   it('should convert case to snake case', () => {
